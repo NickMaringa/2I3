3c8b1e9d04755c533f836e2b9ff36752308dbea6
NickMaringa <106190059+NickMaringa@users.noreply.github.com>
2023-10-29 22:52:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,90 @@
-let lista_nome = ["Isabele","Sara","Alissia","Sophia","Amanda"];
-function atualizar_lista(){
-    let nomes ="";
-
-    if(lista_nome.length > 0){
-        for(let i=0; i < lista_nome.length; i++){
-            nomes += lista_nome[i] + "<br>";
-        }
-        document.getElementById("lista").innerHTML = nomes;
-        return;
-    }
-    document.getElementById("lista").innerHTML = "Lista Vazia";
+let a = "";
+let b = "";
+let valor = "";
+let executar = "";
+let temPonto = false;
+let desligada = false;
+soma = (a,b) => Number(a) + Number(b);
+sub = (a,b) => Number(a) - Number(b);
+mult = (a,b) => Number(a) * Number(b);
+div = (a,b) => Number(a) / Number(b);
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) =>{
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta < 0) return "Não possui raiz Real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
 }
-function insere_primeiro (){
-    let nomes = document.getElementById ("nome").value
-if (nomes!=="");{
-   lista_nome= [];
-    atualizar_lista;
+
+function mostrar_resultado(){
+    document.getElementById("resultado").value = valor;
+}
+function raiz_quadrada(){
+    valor = raiz(valor);
+    mostrar_resultado();
+    valor = "";
+}
+function calcular(){
+    if(desligada) return;
+    if(executar != ""){
+        b = valor;
+        if(executar == "soma") valor = soma(a,b);
+        if(executar == "sub") valor = sub(a,b);
+        if(executar == "div") valor = div(a,b);
+        if(executar == "mult") valor = mult(a,b);
+        mostrar_resultado();
+        executar = "";
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
+    }
+}
+function desliga(){
+    if(desligada){
+        desligada = false;
+        zerar();
+    }else{
+        zerar();
+        mostrar_resultado();
+        desligada = true;
+    }
+}
+
+function porcentagem(){
+   if(executar == "mult"){
+       b = valor;
+       valor = div(mult(a,b),100);
+       mostrar_resultado();
+       valor = "";
    }
- }
-
-function deletar_primeiro (){
-    let nomes = document.getElementById ("nome").value
-if (lista_nome.nome.length > 0) {
-    lista_nome.shift ();
-    document.getElementById("nome").value
-    atualizar_lista ();
 }
- }
-
- function deletar_ultimo (){
-    if (lista_nome.length > 0) {
-        lista_nome.pop ();
-        atualizar_lista ();
-    }
-     }
-     function insere_ultimo (){
-     if(lista_nome.length > 0) {
-       lista_nome.shift();
-       document.getElementById("nomes").value = ""
-       atualizar_lista();
-
-     }
-
-     }
-        
-         function ordenar_za (){
-            if (lista_nome.length > 0) {
-                lista_nome.sort ();
-                lista_nome.reverse ();
-                atualizar_lista ();
-            }
-             }
-             function deletar_nome () {
-        let nome = document.getElementById("nome").value;
-        if(nome != ""){
-            for(let i=0; i < lista_nome,length; i++){
-             if(nome == lista_nome[i]) lista_nome.splice(i,i);
-  }
-        atualizar_lista();
-        }
-
-             }
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    valor = "0";
+    mostrar_resultado();
+    executar = "";
+    valor = "";
+}
+function operacao(op){
+    if(desligada) return;
+    executar = op;
+    a = valor;
+    valor = "";
+}
+function digitando(tecla){
+    if(desligada) return;
+   if (tecla == "."){
+       if(!temPonto) {
+         valor = valor + tecla;
+         mostrar_resultado();
+         temPonto = true;
+       }
+       return;
+   }
+   valor = valor + tecla;
+   mostrar_resultado();
+}
